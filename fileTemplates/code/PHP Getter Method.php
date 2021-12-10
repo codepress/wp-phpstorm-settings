3039188f391e727e152eb42a4a9fceb4ab27f6cf
Tobias Schutter <tobias@codepress.nl>
2021-12-10 14:53:05 +0100

    Compat

--- fileTemplates/code/PHP Getter Method.php
+++ fileTemplates/code/PHP Getter Method.php
@@ -1,7 +1,4 @@
-/**
- * @return ${TYPE_HINT}
- */
-public ${STATIC} function ${GET_OR_IS}_${FIELD_NAME}()#if(${RETURN_TYPE}): ${RETURN_TYPE}#else#end
+public ${STATIC} function ${GET_OR_IS}${NAME}()#if(${RETURN_TYPE}): ${RETURN_TYPE}#else#end
 {
 #if (${STATIC} == "static")
     return self::$${FIELD_NAME};
